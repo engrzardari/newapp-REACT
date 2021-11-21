ea76626a009d5eebb23f0f115dce1ad0db7def36
Engr Saddam Zardari <41242658+engrzardari@users.noreply.github.com>
2021-11-21 08:33:22 -0800

    sample file content

--- src/components/NabBar.js
+++ src/components/NabBar.js
@@ -12,7 +12,7 @@ export class NabBar extends Component {
                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/">QuickNews</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
