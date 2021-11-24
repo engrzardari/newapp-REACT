886d038d989b3b2dbbb788f822f9945f5e088564
Engr Saddam Zardari <41242658+engrzardari@users.noreply.github.com>
2021-11-24 10:26:23 -0800

    routes solution

--- src/App.js
+++ src/App.js
@@ -4,24 +4,17 @@ import React, { Component } from 'react'
 import NabBar from './components/NabBar';
 import News from './components/News';
 import Spinner from './components/Spinner';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
+          
           <NabBar/>
-          <Router>
-            <News pageSize={3} country={'us'} category={'general'}/>
-          </Router>
-    
-          <Switch>
+          <Routes>
             <Route path="/"><News pageSize={3} country={'us'} category={'general'}/></Route>
             <Route path="/business"><News pageSize={3} country={'us'} category={'business'}/></Route>
             <Route path="/entertainment"><News pageSize={3} country={'us'} category={'entertainment'}/></Route>
@@ -30,10 +23,10 @@ export default class App extends Component {
             <Route path="/science"><News pageSize={3} country={'us'} category={'science'}/></Route>
             <Route path="/sports"><News pageSize={3} country={'us'} category={'sports'}/></Route>
             <Route path="/technology"><News pageSize={3} country={'us'} category={'technology'}/></Route>
-          </Switch>
+        </Routes>
+          
       </div>
 
     )
   }
 }
-
